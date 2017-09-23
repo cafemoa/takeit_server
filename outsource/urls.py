70d5909637ff54cd84aa47654ff591825e75ae75
Lee Eunseo <eunseo9808@naver.com>
2017-09-23 19:51:13 +0900

    로그인 수정

--- outsource/urls.py
+++ outsource/urls.py
@@ -1,6 +1,6 @@
 from outsource.views import *
 from django.conf.urls import url,include
-#from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework import routers
 from outsource.views import DeviceViewSet
 
@@ -13,4 +13,5 @@ urlpatterns = [
     url('^', include('outsource.user_urls'), name='user'),
     url(r'^payment_complete/', payment_complete, name='payment_complete'),
     url(r'^payment/', payment, name='payment'),
+    url(r'^api-auth/', obtain_jwt_token),
 ]
